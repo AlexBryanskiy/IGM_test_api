2dd3bd8309bb2b53606dbf9da63fd5987457fa92
Alex Bryanskiy <alexandr.bryanskiy@ukr.net>
2020-01-09 11:31:37 +0200

    - create ContactController class; - create Contact model class; - add README.md file;

---
+++ src/main/java/com/example/demo/model/Contact.java
@@ -0,0 +1,81 @@
+package com.example.demo.model;
+
+public class Contact {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String phoneNumber;
+    private String cellPhoneNumber;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCellPhoneNumber() {
+        return cellPhoneNumber;
+    }
+
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", cellPhoneNumber='" + cellPhoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
